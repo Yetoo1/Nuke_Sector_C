4c5d7277d132c687310e1465f3a8f817ce83c5d0
yetoo-server <yetoohappy@gmail.com>
2017-01-26 14:19:50 -0500

    fucking added shit

--- main.c
+++ main.c
@@ -30,6 +30,7 @@ int main (int argc, char *argv[])
 	srand(time(NULL)); //starts the instance of random
 	/* The arrays that store shit starts here */
 	char sectorletter[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}; //will need to convert to string
+	//char sectorletter[26] =  {"A","B","C","D","E","F","G","H","I","J","K", "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}; //this is the array that is supposed to hold the predefined letters that sectors will contain 
 	/* The arrays that store shit ends here */
 	int sectoramnt = rand() % 100 + 1; //sectoramnt equals a random number from 1 to 100 (verify on that) 
 	printw("Sectors:\n");
@@ -39,8 +40,12 @@ int main (int argc, char *argv[])
 	{
 		int sector = rand() % 100 + 1; //sector equals a random number from 1 to 100 (verify on that)
 		char sectorts[3];
-		sprintf(sectorts, "%d", sector);
-		//char sectorwl 
+		char sectorwl[4];
+		sprintf(sectorts, "%d", sector); //converts the sector number to string
+		sectorwl[0] = sectorletter[rand()%26+0] + '\0';//make the zero position the char that's converted
+		//memcpy(sectorwl, sectorletter[rand()%26+0], 1);
+		//printf("%s\n", sectorletter);
+		//char sectorwl = sectorletter[rand()%26+0] 
 	}
 	while ((open = getch()) != 'q')
 	{
@@ -49,5 +54,6 @@ int main (int argc, char *argv[])
 		//break;
 	}
 	refresh();
+	curs_set(TRUE);
 	endwin();
 }
