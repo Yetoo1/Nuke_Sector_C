2fefc0c10209fc0a3de9654db95effdadbb5288e
yetoo-home-server <yetoohappy@gmail.com>
2017-01-29 09:41:56 -0800

    Made multiple lines in c++ 1 line in c, i feel like a shitty badass.

--- main.c
+++ main.c
@@ -29,20 +29,18 @@ int main (int argc, char *argv[])
 	//curs_set(FALSE);
 	//refresh();
 	srand(time(NULL)); //starts the instance of random
-	/* The arrays that store shit starts here */
-	//char sectorletter[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}; //will need to convert to string
 	char *sectorletter[26] =  {"A","B","C","D","E","F","G","H","I","J","K", "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}; //this is the array that is supposed to hold the predefined letters that sectors will contain 
 	/* The arrays that store shit ends here */
 	int sectoramnt = rand() % 100 + 1; //sectoramnt equals a random number from 1 to 100 (verify on that) 
+	char *sectorindex[sectoramnt];
 	printf("Sectors:\n");
 	/* THE LOOP THAT PUTS VARIABLES INTO THE INDEX STARTS HERE */
-	char *sectorln;
 	int i;
 	for (i = 0; i < sectoramnt; i++) //for loop iterates for whatever number of times defined by sectoramnt
 	{
 		int sector = rand() % 100 + 1; //sector equals a random number from 1 to 100 (verify on that)
-		asprintf(&sectorln, "%s%d", sectorletter[rand()%26+0], sector);
-		printf("%s ", sectorln);
+		asprintf(&sectorindex[i], "%s%d", sectorletter[rand()%26+0], sector); //this took me for fucking ever to figure this out
+		printf("%s ", sectorindex[i]);
 	}
 	printf("\n");
 	int happy = 0;
