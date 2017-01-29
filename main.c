ea24e4f12ed0de36952c62e011c036fef98e502f
yetoo-home-server <yetoohappy@gmail.com>
2017-01-29 09:26:20 -0800

    Made the concatenation of the string less retarded

--- main.c
+++ main.c
@@ -30,26 +30,18 @@ int main (int argc, char *argv[])
 	//refresh();
 	srand(time(NULL)); //starts the instance of random
 	/* The arrays that store shit starts here */
-	char sectorletter[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}; //will need to convert to string
-	//char sectorletter[26] =  {"A","B","C","D","E","F","G","H","I","J","K", "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}; //this is the array that is supposed to hold the predefined letters that sectors will contain 
+	//char sectorletter[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}; //will need to convert to string
+	char *sectorletter[26] =  {"A","B","C","D","E","F","G","H","I","J","K", "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}; //this is the array that is supposed to hold the predefined letters that sectors will contain 
 	/* The arrays that store shit ends here */
 	int sectoramnt = rand() % 100 + 1; //sectoramnt equals a random number from 1 to 100 (verify on that) 
 	printf("Sectors:\n");
 	/* THE LOOP THAT PUTS VARIABLES INTO THE INDEX STARTS HERE */
+	char *sectorln;
 	int i;
 	for (i = 0; i < sectoramnt; i++) //for loop iterates for whatever number of times defined by sectoramnt
 	{
-		//for some reason this part outputs garbled characters with some versions of gcc, the one on my laptop works though
 		int sector = rand() % 100 + 1; //sector equals a random number from 1 to 100 (verify on that)
-		char sectorts[3];
-		char sectorlc[3]; //converted letter
-		char *sectorln; //sector with number and letter
-		sprintf(sectorts, "%d", sector); //converts the sector number to string
-		sectorlc[0] = sectorletter[rand()%26+0] + '\0';//make the zero position the char that's converted
-		asprintf(&sectorln, "%s%s", sectorlc, sectorts);
-		//memcpy(sectorwl, sectorletter[rand()%26+0], 1);
-		//printf("%s\n", sectorletter);
-		//char sectorwl = sectorletter[rand()%26+0] 
+		asprintf(&sectorln, "%s%d", sectorletter[rand()%26+0], sector);
 		printf("%s ", sectorln);
 	}
 	printf("\n");
@@ -60,10 +52,10 @@ int main (int argc, char *argv[])
 		char sectordec[5];
 		fgets(sectordec, 5, stdin);
 		printf("You selected sector %s\n", sectordec);	
-		for (i = 0; u < sectoramnt; i++)
-		{
-			if (strcmp(sectordec, sectorindex[i]))
-		}	
+		//for (i = 0; u < sectoramnt; i++)
+		//{
+		//	if (strcmp(sectordec, sectorindex[i]))
+		//}	
 	}	
 	//char input[4];	
 	//while ((open = getch()) != 'q')
